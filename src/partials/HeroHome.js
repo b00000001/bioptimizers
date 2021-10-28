a6620d0ee6822e28718412bcf30d8c2c6950bf6a
Aaron Deas <deas.aaron@gmail.com>
2021-10-28 14:36:04 -0400

    Made more cosmetic touches, edited social links in footer

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -66,14 +66,6 @@ function HeroHome({ handleClick }) {
                 data-aos="zoom-y-out"
                 data-aos-delay="300"
               >
-                {/* <div>
-                  <a
-                    className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0"
-                    href="#0"
-                  >
-                    Start free trial
-                  </a>
-                </div> */}
                 <div>
                   <a
                     className="btn text-white bg-green-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
@@ -101,73 +93,6 @@ function HeroHome({ handleClick }) {
                   height="432"
                   alt="Hero"
                 />
-                {/* <svg
-                  className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto"
-                  width="768"
-                  height="432"
-                  viewBox="0 0 768 432"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                >
-                  <defs>
-                    <linearGradient
-                      x1="50%"
-                      y1="0%"
-                      x2="50%"
-                      y2="100%"
-                      id="hero-ill-a"
-                    >
-                      <stop stopColor="#FFF" offset="0%" />
-                      <stop stopColor="#EAEAEA" offset="77.402%" />
-                      <stop stopColor="#DFDFDF" offset="100%" />
-                    </linearGradient>
-                    <linearGradient
-                      x1="50%"
-                      y1="0%"
-                      x2="50%"
-                      y2="99.24%"
-                      id="hero-ill-b"
-                    >
-                      <stop stopColor="#FFF" offset="0%" />
-                      <stop stopColor="#EAEAEA" offset="48.57%" />
-                      <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />
-                    </linearGradient>
-                    <radialGradient
-                      cx="21.152%"
-                      cy="86.063%"
-                      fx="21.152%"
-                      fy="86.063%"
-                      r="79.941%"
-                      id="hero-ill-e"
-                    >
-                      <stop stopColor="#4FD1C5" offset="0%" />
-                      <stop stopColor="#81E6D9" offset="25.871%" />
-                      <stop stopColor="#338CF5" offset="100%" />
-                    </radialGradient>
-                    <circle id="hero-ill-d" cx="384" cy="216" r="64" />
-                  </defs>
-                  <g fill="none" fillRule="evenodd">
-                    <circle
-                      fillOpacity=".04"
-                      fill="url(#hero-ill-a)"
-                      cx="384"
-                      cy="216"
-                      r="128"
-                    />
-                    <circle
-                      fillOpacity=".16"
-                      fill="url(#hero-ill-b)"
-                      cx="384"
-                      cy="216"
-                      r="96"
-                    />
-                    <g fillRule="nonzero">
-                      <use fill="#000" xlinkHref="#hero-ill-d" />
-                      <use fill="url(#hero-ill-e)" xlinkHref="#hero-ill-d" />
-                    </g>
-                  </g>
-                </svg>
-              </div> */}
               </div>
               <button
                 className="bg-green-700 absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg"
